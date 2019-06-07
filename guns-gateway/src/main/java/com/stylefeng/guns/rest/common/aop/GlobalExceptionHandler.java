4e3079e108bdddf8c156e664ae7092a2177c3553
EthanNew <shiminniu@163.com>
2019-06-07 23:17:17 +0800

    修改了全局的异常拦截器

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/common/aop/GlobalExceptionHandler.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/common/aop/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.stylefeng.guns.rest.common.aop;
 import com.stylefeng.guns.core.aop.BaseControllerExceptionHandler;
 import com.stylefeng.guns.core.base.tips.ErrorTip;
 import com.stylefeng.guns.rest.common.exception.BizExceptionEnum;
+import com.stylefeng.guns.rest.model.cinema.responsevo.ResponseExceptionVO;
 import io.jsonwebtoken.JwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,5 +32,22 @@ public class GlobalExceptionHandler extends BaseControllerExceptionHandler {
     @ResponseBody
     public ErrorTip jwtException(JwtException e) {
         return new ErrorTip(BizExceptionEnum.TOKEN_ERROR.getCode(), BizExceptionEnum.TOKEN_ERROR.getMessage());
+    }
+
+    /**
+     * 处理所有不可知的异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseExceptionVO handleException(Exception e) {
+        log.error(e.getMessage(), e);
+
+        ResponseExceptionVO response = new ResponseExceptionVO();
+        response.setStatus(999);
+        response.setMsg("系统出现异常，联系管理员！");
+        return response;
     }
 }
